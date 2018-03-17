502ed2e518eb87e98af04114da4fdcec443ed133
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-03-17 10:09:44 +0100

    Added initial version of post request to the api.

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const apiKey = process.env.WEATHERAPP_API_KEY
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 
 app.get('/', function (req, res) {
     res.render('index')
+})
+
+app.post('/', function (req, res) {
+    const city = req.body.city;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    request(url, function (err, response, body) {
+        if (err) {
+            res.render('index', { weather: null, error: 'Error, please try again' });
+        } else {
+            const weather = JSON.parse(body)
+            if (weather.main == undefined) {
+                res.render('index', { weather: null, error: 'Error, please try again' });
+            } else {
+                const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.render('index', { weather: weatherText, error: null });
+            }
+        }
+    });
 })
 
 app.listen(3000, function () {
