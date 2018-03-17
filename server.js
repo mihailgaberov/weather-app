329928c1fc15058fce4f7116aca59c18348c80d4
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-03-17 12:21:00 +0100

    Finalizing the initial version of the app. Updated the template and api call.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
+require('dotenv').config()
+
 const apiKey = process.env.WEATHERAPP_API_KEY
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -17,14 +20,15 @@ app.post('/', function (req, res) {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
     request(url, function (err, response, body) {
         if (err) {
-            res.render('index', { weather: null, error: 'Error, please try again' });
+            res.render('index', { weather: null, error: 'Error, please try again' })
         } else {
             const weather = JSON.parse(body)
+            console.log(weather)
             if (weather.main == undefined) {
-                res.render('index', { weather: null, error: 'Error, please try again' });
+                res.render('index', { weather: null, error: 'Error, please try again' })
             } else {
-                const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-                res.render('index', { weather: weatherText, error: null });
+                const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
+                res.render('index', { weather: weatherText, error: null })
             }
         }
     });
