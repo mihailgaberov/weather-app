e70d088136a434c8ad293c40afc9ae32bd8c6773
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-03-17 13:38:26 +0100

    Added favicon

--- server.js
+++ server.js
@@ -11,28 +11,27 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-    res.render('index')
+  res.render('index')
 })
 
 app.post('/', function (req, res) {
-    const city = req.body.city;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-    request(url, function (err, response, body) {
-        if (err) {
-            res.render('index', { weather: null, error: 'Error, please try again' })
-        } else {
-            const weather = JSON.parse(body)
-            console.log(weather)
-            if (weather.main === undefined) {
-                res.render('index', { weather: null, error: 'Error, please try again' })
-            } else {
-                const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
-                res.render('index', { weather: weatherText, error: null })
-            }
-        }
-    });
+  const city = req.body.city;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  request(url, function (err, response, body) {
+    if (err) {
+      res.render('index', { weather: null, error: 'Error, please try again' })
+    } else {
+      const weather = JSON.parse(body)
+      if (weather.main === undefined) {
+        res.render('index', { weather: null, error: 'Error, please try again' })
+      } else {
+        const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
+        res.render('index', { weather: weatherText, error: null })
+      }
+    }
+  })
 })
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!')
 })
