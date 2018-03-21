a6e8da2f1015339660138f6cf45e5ca69cf7381a
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-03-21 07:00:47 +0100

    Added cron job and sending email warning every mornings with the temperature in a selected city.

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
+const sgMail = require('@sendgrid/mail');
+const CronJob = require('cron').CronJob
 require('dotenv').config()
 
 const apiKey = process.env.WEATHERAPP_API_KEY
 const port = process.env.PORT || 8080
+const MY_EMAIL = process.env.MY_EMAIL
+const HER_EMAIL = process.env.HER_EMAIL
+const timeZone = 'Europe/Zurich'
+const emailCity = 'Zurich'
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -26,7 +32,7 @@ app.post('/', function (req, res) {
       if (!weather || weather.main === undefined) {
         res.render('index', { weather: null, error: 'Error, please try again' })
       } else {
-        const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
+        const weatherText = `It's ${Math.round(parseInt(weather.main.temp))} degrees in ${weather.name}!`
         res.render('index', { weather: weatherText, error: null })
       }
     }
@@ -36,3 +42,39 @@ app.post('/', function (req, res) {
 app.listen(port, function () {
   console.log('Server running on port: ', port)
 })
+
+
+function sendEmail() {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${emailCity}&units=metric&appid=${apiKey}`
+  request(url, function (err, response, body) {
+    if (err) {
+      throw new Error('Error, please try again')
+    } else {
+      const weather = JSON.parse(body)
+      if (!weather || weather.main === undefined) {
+        throw new Error('Error, please try again')
+      } else {
+        const roundedWeather = Math.round(parseInt(weather.main.temp))
+        console.log('roundedWeather= ', roundedWeather)
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+          to: [MY_EMAIL, HER_EMAIL],
+          from: 'my@weatherapp.com',
+          subject: `The weather today in ${emailCity}`,
+          text: `Your Majesty, The weather today in ${emailCity} will be ${roundedWeather}`,
+          html: `<p><i>Your Majesty</i>, The weather today in <b>${emailCity}</b> will be <b style='color: orangered;'>${roundedWeather}</b></p>`,
+        };
+        sgMail.send(msg);
+      }
+    }
+  })
+}
+
+const job = new CronJob('00 30 07 * * 1-5', function () {
+    sendEmail()
+  }, function () {
+    console.log('stopped...')
+  },
+  true,
+  timeZone
+)
